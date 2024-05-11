a1652e65a3dd043e5d3b06645953a931c53688b9
LudovicaMassimino <ludovica.massimino93@gmail.com>
2024-05-11 13:45:24 +0200

    commentato le righe scritte per la consegna precedente e testato Prodotto

--- src/org/lessons/java/shop/inheritance/Main.java
+++ src/org/lessons/java/shop/inheritance/Main.java
@@ -1,22 +1,23 @@
 package org.lessons.java.shop.inheritance;
 
-import org.lessons.java.shop.inheritance.Smartphone;
-
 public class Main {
 	public static void main(String[] args) {
-		Smartphone mySmartphone = new Smartphone("Iphone", "Apple", 900.00, "123456789012345", 32);
+		//Smartphone mySmartphone = new Smartphone("Iphone", "Apple", 900.00, "123456789012345", 32);
 //		System.out.println("Nome del prodotto: " + mySmartphone.getNome());
 //        System.out.println("Marca del prodotto: " + mySmartphone.getMarca());
 //        System.out.println("Prezzo base: " + "€ " + mySmartphone.getPrezzo());
 //        System.out.println("Prezzo compreso di IVA al 22%: " + "€ " + mySmartphone.prezzoConIva());
 //        System.out.println("codice: " + mySmartphone.codice);
 		
-		System.out.println(mySmartphone.toString());
+		//System.out.println(mySmartphone.toString());
 		
-		Televisori myTelevisori = new Televisori("UE40", "Samsung", 400.00 ,30, true);
+		/*Televisori myTelevisori = new Televisori("UE40", "Samsung", 400.00 ,30, true);
 		System.out.println(myTelevisori.toString());
 		
 		Cuffie myCuffie = new Cuffie("tuneBM","JBL", 150.00, "blu", "wireless");
-		System.out.println(myCuffie.toString());
+		System.out.println(myCuffie.toString());*/
+		
+		Prodotto myProdotto = new Prodotto("Prodotto1", "Marca1", 100.00);
+		System.out.println(myProdotto);
 	}
 }
